15b7ea07ef26619ea971e6db40389f865c3c864b
Kewell Chen <chao_rap@163.com>
2018-12-22 23:20:26 +0800

    Multiple parameter OK

---
+++ Gradient Descent/多系数-梯度下降2.py
@@ -0,0 +1,52 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Initail value Y = W*X + B
+W = -8.6
+B = 10
+Step = 0.0001
+
+Ytrain = np.array([-2.1, -1.2, 2.2, 3.0], dtype="double")
+Xtrain = np.array([-1.9, -1.3, 2.1, 3.1], dtype="double")
+
+Size = Xtrain.size
+
+plt.scatter(Xtrain,Ytrain)
+
+def CalcError(w, b):
+    err = 0
+    for i in range(0, Size-1):
+        err += ((Xtrain[i]*w + b - Ytrain[i])*(Xtrain[i]*w + b - Ytrain[i]))
+    return err
+
+def CalcErrorWDaoShu(w, b):
+    ds = 0
+    for i in range(0, Size-1):
+        ds += 2*(Xtrain[i]*w + b - Ytrain[i])*Xtrain[i]
+    return ds
+
+def CalcErrorBDaoShu(w, b):
+    ds = 0
+    for i in range(0, Size-1):
+        ds += 2*(Xtrain[i]*w + b - Ytrain[i])
+    return ds
+
+NewError = 0
+OldError = CalcError(W, B)
+
+while 1>0:
+    W = W - Step*CalcErrorWDaoShu(W, B)
+    B = B - Step*CalcErrorBDaoShu(W, B)
+    print(NewError, W, B)
+    OldError = NewError
+    NewError = CalcError(W, B)
+    if(abs(NewError - OldError) < 0.000000000001):
+        break
+    
+print(W, B)
+
+Xtest = np.arange(-2, 2, 0.001)
+Ytest = W*Xtest + B
+plt.plot(Xtest, Ytest)
+
+plt.show()
