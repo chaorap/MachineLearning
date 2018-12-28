ec46f013ce2ee72bd621207e980bd552485eb0f9
Kewell-YiChao Chen <kewell-yichao.chen@cn.abb.com>
2018-12-28 11:01:48 +0800

    Loaded successful and Can display loaded data to picture

--- MNIST/train.py
+++ MNIST/train.py
@@ -1,6 +1,7 @@
 import numpy as np
 import struct
 import matplotlib.pyplot as plt
+from PIL import Image
 
 FileName_TrainingImage = "train-images.idx3-ubyte"
 FileName_TrainingLabel = "train-labels.idx1-ubyte"
@@ -17,7 +18,7 @@ Ytrain = np.zeros(shape=(PreDefine_TrainingNumber, 10))
 Xtest = np.zeros(shape=(PreDefine_TestNumber, 28*28))
 Ytest = np.zeros(shape=(PreDefine_TestNumber, 10))
 
-def Load_MNIST_DataSet(Xtrain, Ytrain, Xtest, Ytest):
+def Load_MNIST_DataSet(Xtrain, Ytrain, Xtest, Ytest, limitNumber):
     for filename in ["train-images.idx3-ubyte",  "train-labels.idx1-ubyte",  "t10k-images.idx3-ubyte",   "t10k-labels.idx1-ubyte"]:
         try:
             f = open(filename, "rb")
@@ -28,29 +29,53 @@ def Load_MNIST_DataSet(Xtrain, Ytrain, Xtest, Ytest):
                 RowNumber = struct.unpack(">i", f.read(4))[0]
                 ColNumber = struct.unpack(">i", f.read(4))[0]
 
-                for i in range(0, TrainNumber - 1):
-                    for j in range(0, RowNumber * ColNumber - 1):
+                for i in range(0, limitNumber):
+                    print("%s - %d/%d" %(filename, i+1, TrainNumber))
+                    for j in range(0, RowNumber * ColNumber):
                         if "train" in filename:
                             Xtrain[i][j] = struct.unpack("B", f.read(1))[0]
                         else:
                             Xtest[i][j] = struct.unpack("B", f.read(1))[0]
             else:
-                for i in range(0, TrainNumber - 1):
+                for i in range(0, limitNumber):
+                    print("%s - %d/%d" % (filename, i+1, TrainNumber))
                     if "train" in filename:
                         Ytrain[i][struct.unpack("B", f.read(1))[0]] = 1;
                     else:
                         Ytest[i][struct.unpack("B", f.read(1))[0]] = 1;
-
-            #------------Debug code for display picture----------------
-            #img = np.zeros(shape=(RowNumber,ColNumber))
-            #for i in range(0, RowNumber):
-            #    for j in range(0, ColNumber):
-            #        img[i][j] = struct.unpack("B", f.read(1))[0]
-            #plt.imsave('output.tiff', img, format='tiff', cmap=plt.cm.gray)
         except IOError:
             print("Cannot open " + filename)
-            return 0
+            return false
+    return True
+
+def DisplayLoadedMNISTPicture(PictureNumber, IsTrain=True):
+    img = np.zeros(shape=(28,28))
+    label = -100
+    for i in range(0, 28):
+        for j in range(0, 28):
+            if IsTrain == True:
+                img[i][j] = Xtrain[PictureNumber][i*28+j]
+            else:
+                img[i][j] = Xtest[PictureNumber][i*28+j]
+    for i in range(0, 10):
+        if IsTrain == True:
+            if Ytrain[PictureNumber][i] > 0:
+                label = i;
+                break;
+        else:
+            if Ytest[PictureNumber][i] > 0:
+                label = i;
+                break;
+
+    if label >= 0:
+        imgName = "Number_%d_Label_%d.tiff" %(PictureNumber, label)
+        plt.imsave(imgName, img, format='tiff', cmap=plt.cm.gray)
+        img=Image.open(imgName)
+        plt.figure(imgName)
+        plt.imshow(img)
+        plt.show()
 
 if __name__ == "__main__":
-    Load_MNIST_DataSet(Xtrain, Ytrain, Xtest, Ytest)
-    print(Xtrain)
+    if Load_MNIST_DataSet(Xtrain, Ytrain, Xtest, Ytest, 500):
+        DisplayLoadedMNISTPicture(0, True)
+        DisplayLoadedMNISTPicture(1, False)
